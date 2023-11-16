20a9aa21bd644b0b323ab5b70386d3f055b6e258
GilangAnnugroho <148997996+GilangAnnugroho@users.noreply.github.com>
2023-11-16 12:08:30 +0700

    Add files via upload

---
+++ Pertemuan 4/Modul Bangun Ruang/Import/balok.py
@@ -0,0 +1,7 @@
+def luas_balok (p,l,t) :
+    luas_balok = round (2*p*l) + (2*p*t) + (2*l*t)
+    return luas_balok
+
+def volume_balok (p,l,t) :
+    volume_balok = round (p*l*t)
+    return volume_balok
---
+++ Pertemuan 4/Modul Bangun Ruang/Import/kubus.py
@@ -0,0 +1,7 @@
+def luas_kubus (r) :
+    luas_kubus = round (6 * (r ** 2))
+    return luas_kubus
+
+def volume_kubus (r) :
+    volume_kubus = round ( r ** 3)
+    return volume_kubus
